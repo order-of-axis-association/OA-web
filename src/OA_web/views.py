2f4c9862bc50fc2fe3d22f4359c618bac5c853f3
Yuto Takamoto <scarletjaeger@gmail.com>
2019-12-15 23:09:12 +0000

    Woops didn't do mobile first

--- src/OA_web/views.py
+++ src/OA_web/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
-    return HttpResponse("Hello World");
+    template = loader.get_template('templates/wip.html')
+    return HttpResponse(template.render({}, request));
