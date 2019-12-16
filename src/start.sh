9b1ad86fa3d64b97c7fa8428effcdfce683ee30d
Yuto Takamoto <scarletjaeger@gmail.com>
2019-12-16 00:53:50 +0000

    What is responsive design???

--- src/start.sh
+++ src/start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+VENV_PY="/home/remi/development/OA_web/DEV/bin/python3"
 
 export LD_LIBRARY_PATH=/usr/local/lib
-./manage.py runserver 0.0.0.0:8000
+${VENV_PY} manage.py runserver 0.0.0.0:80
